f07bd6a5ec0286a5d4f6d7a31a1eb95361ab708f
RobertWenzel <58034082+RobertWenzel@users.noreply.github.com>
2020-09-09 08:22:02 +0200

    1

--- application.py
+++ application.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask,request, Response
+import pip
+import json
+import os, uuid
+
+from azure.storage.queue import (
+        QueueClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    
+    return Response(response="Hello World", status=200)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    with open("data.txt", "w") as outfile:
+        json.dump(request.json,outfile)
+    validationToken = request.args.get("validationToken")
+    return Response(response=validationToken, status=200)
+
